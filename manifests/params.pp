6ea273601c50f6f21ab3da0aa64e1a8cb7894712
Jared Jennings <jjennings@fastmail.fm>
2015-08-01 16:42:41 -0500

    add support for RHEL and CentOS

--- manifests/params.pp
+++ manifests/params.pp
@@ -19,6 +19,18 @@ class xserver::params {
         }
       }
     }
+
+    'RedHat', 'CentOS': {
+      case $::operatingsystemmajrelease {
+        '5', '6', '7': {
+          $xserver_package = 'xorg-x11-server-Xorg'
+          $xserver_xorg_conf_file = '/etc/X11/xorg.conf'
+        }
+        default: {
+          fail("Unsupported platform: ${module_name} currently doesn't support ${::operatingsystem} ${::operatingsystemmajrelease}")
+        }
+      }
+    }
     
     default: {
       fail("Unsupported platform: ${module_name} currently doesn't support ${::operatingsystem}")
